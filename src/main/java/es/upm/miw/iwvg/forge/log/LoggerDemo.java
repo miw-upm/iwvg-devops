d88ce9fea040b70ee7d719de690bfb27b2bed331
JBernal <j.bernal@upm.es>
2017-09-16 17:59:45 +0200

    refactoring

--- src/main/java/es/upm/miw/iwvg/forge/log/LoggerDemo.java
+++ src/main/java/es/upm/miw/iwvg/forge/log/LoggerDemo.java
@@ -1,13 +1,9 @@
 package es.upm.miw.iwvg.forge.log;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class LoggerDemo {
-    private List<Double> collection = new ArrayList<>();
 
     public void logs() {
         Logger logger = LogManager.getLogger(this.getClass().getName());
@@ -17,18 +13,6 @@ public class LoggerDemo {
         logger.info("Log de info");
         logger.debug("Log de debug");
         logger.trace("Log de trace");
-        
-        double higher = Double.NEGATIVE_INFINITY;
-        for (double item : this.collection) {
-            if (item > higher) {
-                higher = item;
-            }
-        }
-
-    }
-
-    public static void main(String[] args) {
-        new LoggerDemo().logs();
     }
 
 }
