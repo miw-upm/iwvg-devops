8ed4414d506b830fad3422b9b51583dc711d5715
jbernal <jbernal@etsisi.upm.es>
2020-05-10 18:51:32 +0200

    updated to webflux

--- src/main/java/es/upm/miw/iwvg_devops/api/SecurityConfiguration.java
+++ src/main/java/es/upm/miw/iwvg_devops/api/SecurityConfiguration.java
@@ -1,21 +1,25 @@
 package es.upm.miw.iwvg_devops.api;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableReactiveMethodSecurity
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        return http
+                .formLogin().disable()
+                .csrf().disable()
+                .logout().disable()
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+                .build();
     }
-
 }
--- src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
@@ -2,7 +2,7 @@ package es.upm.miw.iwvg_devops.code;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,19 +11,19 @@ class SearchesTest {
 
     @Test
     void testFindUserFamilyNameByUserNameDistinct() {
-        assertEquals(Arrays.asList("Torres"), new Searches().findUserFamilyNameByUserNameDistinct("Paula")
+        assertEquals(List.of("Torres"), new Searches().findUserFamilyNameByUserNameDistinct("Paula")
                 .collect(Collectors.toList()));
     }
 
     @Test
     void testFindUserFractionNumeratorByFamilyName() {
-        assertEquals(Arrays.asList(2, 4, 0, 1, 1), new Searches().findUserFractionNumeratorByFamilyName("Torres")
+        assertEquals(List.of(2, 4, 0, 1, 1), new Searches().findUserFractionNumeratorByFamilyName("Torres")
                 .collect(Collectors.toList()));
     }
 
     @Test
     void testFindFamilyNameByFractionDenominator() {
-        assertEquals(Arrays.asList("López", "Torres"), new Searches().findFamilyNameByFractionDenominator(2)
+        assertEquals(List.of("López", "Torres"), new Searches().findFamilyNameByFractionDenominator(2)
                 .collect(Collectors.toList()));
     }
 
